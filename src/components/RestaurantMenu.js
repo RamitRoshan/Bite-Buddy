df4655591c8591e09aa81a65692a0d0b39267dfd
Ramit Roshann <ramitroshan777777@gmail.com>
2025-06-08 02:39:38 +0530

    style the cuisions

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -80,8 +80,8 @@ const RestaurantMenu = () =>{
             <ul>
                 {itemCards.map((item) => (
                     <li key={item.card.info.id}>
-                        {item.card.info.name} - {"Rs."}
-                        {item.card.info.price/100 || item.card.info.defaultprice/100}
+                 {item.card.info.name} - {"Rs."}
+                 {item.card.info.price/100 || item.card.info.defaultprice/100}
                     </li>
                 ))}
             </ul> */}
